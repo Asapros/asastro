779b9f4d10323928ea879a93e6e7470afb84c007
Asapros <cyanowakredka@gmail.com>
2025-05-23 12:41:25 +0200

    Add two balls

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,23 @@ fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);
 }
 
-fn test_spawn_shape(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
+fn test_spawn_shapes(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
     let mesh = Mesh2d(meshes.add(Circle::new(50.0)));
-    let material = MeshMaterial2d(materials.add(Color::srgb(1.0, 1.0, 1.0)));
+    let material_red = MeshMaterial2d(materials.add(Color::srgb(1.0, 0.0, 0.0)));
     commands.spawn((
-        mesh, material
+        mesh.clone(), material_red.clone(), Transform::from_xyz(-100.0, 0.0, 0.0)
+    )
+    );
+    let material_green = MeshMaterial2d(materials.add(Color::srgb(0.0, 1.0, 0.0)));
+    commands.spawn((
+        mesh.clone(), material_green.clone(), Transform::from_xyz(100.0, 0.0, 0.0)
     ));
-    
 }
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup_camera)
-        .add_systems(Startup, test_spawn_shape)
+        .add_systems(Startup, test_spawn_shapes)
         .run();
 }
