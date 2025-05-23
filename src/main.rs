d63c120e4e3df9095bd29514dea4fc4b0d9d8a26
Asapros <cyanowakredka@gmail.com>
2025-05-23 12:30:52 +0200

    Add barebones bevy setup

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use bevy::prelude::*;
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup_camera)
+        .run();
+}
