40ed06193fa412cf44c855ce5764028c61f4c0e0
Asapros <cyanowakredka@gmail.com>
2025-06-02 19:51:58 +0200

    Add help text

--- src/view/diagnostics.rs
+++ src/view/diagnostics.rs
@@ -6,11 +6,31 @@ use crate::view::follow::FollowInfo;
 #[derive(Component)]
 struct DiagnosticsText;
 
+#[derive(Component)]
+struct HelpText;
+
+const HELP_TEXT: &'static str = r"========== CONTROLS ==========
+move around - drag with RIGHT MB
+zoom in/out - SCROLL
+reset view - Z
+toggle scale - N
+toggle pause - SPACE
+slow down simulation - ,
+speed up simulation - .
+reverse time - ;
+setting frame of reference -
+digit keys (1: Sun, ..., 9: Neptune) <<<
+or click LEFT MB on a body <<<
+
+speeding up too much messes up the orbits !!!
+SPS is self-stabilising, so low FPS can also break !!!
+";
+
 fn setup_text(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
         Text::new(""),
         TextFont {
-            font_size: 32.0,
+            font_size: 15.0,
             ..default()
         },
         TextShadow::default(),
@@ -24,6 +44,24 @@ fn setup_text(mut commands: Commands, asset_server: Res<AssetServer>) {
         DiagnosticsText
     ));
 
+
+    commands.spawn((
+        Text::new(HELP_TEXT),
+        TextFont {
+            font_size: 15.0,
+            ..default()
+        },
+        TextShadow::default(),
+        TextLayout::new_with_justify(JustifyText::Right),
+        Node {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(5.0),
+            right: Val::Px(5.0),
+            ..default()
+        },
+        HelpText,
+        Visibility::Visible
+    ));
 
 }
 
@@ -51,7 +89,7 @@ fn time_to_string(years: f32) -> String {
     return format!("{:.1} seconds", minutes * 60.0);
 }
 
-fn update_text(mut text: Query<&mut Text, With<DiagnosticsText>>, diagnostics: Res<DiagnosticsStore>, settings: Res<SimulationSettings>, follow_info: Res<FollowInfo>) {
+fn update_diagnostic_text(mut text: Query<&mut Text, With<DiagnosticsText>>, diagnostics: Res<DiagnosticsStore>, settings: Res<SimulationSettings>, follow_info: Res<FollowInfo>) {
     let mut text = text.single_mut().expect("Diagnostic text not found");
     let fps = diagnostics.get(&FrameTimeDiagnosticsPlugin::FPS).and_then(|d| d.average());
     let fps_text = match fps {
@@ -74,7 +112,7 @@ fn update_text(mut text: Query<&mut Text, With<DiagnosticsText>>, diagnostics: R
         None => "Reference: -".to_string(),
         Some(name) => format!("Reference: {}", name)
     };
-    
+
     let normalized_text = match &settings.normalized {
         true => "Scale: normalized",
         false => "Scale: true"
@@ -83,10 +121,19 @@ fn update_text(mut text: Query<&mut Text, With<DiagnosticsText>>, diagnostics: R
     text.0 = [sps_text_with_pause, following_text, normalized_text].join("\n");
 }
 
+fn update_help_text(mut text: Query<&mut Visibility, With<HelpText>>, settings: Res<SimulationSettings>) {
+    let mut help = text.single_mut().expect("Help text not found");
+    *help = match settings.pause {
+        true => Visibility::Visible,
+        false => Visibility::Hidden
+    }
+}
+
 pub(super) struct DiagnosticsPlugin;
 impl Plugin for DiagnosticsPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup_text);
-        app.add_systems(Update, update_text);
+        app.add_systems(Update, update_help_text);
+        app.add_systems(Update, update_diagnostic_text);
     }
 }
